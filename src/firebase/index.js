1464caca5dcdc920de83d808949c9efa033ef7bf
aida <aidamajid117@gmail.com>
2019-01-16 10:57:52 +0800

    adding files

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+
+import firebase from 'firebase';
+
+  const config = {
+    apiKey: "AIzaSyD7NQ2eERikiAFHjqcfG5mdlK67NaM0Bsk",
+    authDomain: "meal-prep-a8fe3.firebaseapp.com",
+    databaseURL: "https://meal-prep-a8fe3.firebaseio.com",
+    projectId: "meal-prep-a8fe3",
+    storageBucket: "meal-prep-a8fe3.appspot.com",
+    messagingSenderId: "660903928497"
+  };
+  firebase.initializeApp(config);
